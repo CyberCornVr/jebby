870084f478e20134ba227ef881490fcf9a4f2c05
CyberCorn <81420886+CyberCornVr@users.noreply.github.com>
2023-04-03 10:51:35 -0500

    Update gui.js

--- unobfuscated/gui.js
+++ unobfuscated/gui.js
@@ -1,3 +1,10 @@
-fetch("https://api.github.com/repos/CyberCornVr/jebby/releases/latest")
-  .then(data => window.alert(data.tag_name))
-  .catch(error => console.error(error));
+function CheckVersion() {
+  let x = await fetch("https://raw.githubusercontent.com/CyberCornVr/jebby/main/version/version.txt");
+  let y = await x.text();
+  if(y > 1.0) {
+    let z = window.confirm(The Code you are using is outdated!\nTo use the latest version press OK\nTo use your current version press cancel);
+    if(z = true) {
+      window.location.href = "https://github.com/CyberCornVr/jebby"
+    }
+  }
+}
